f0c6f1bdd09171d47fcd873d020b3e48bf37c7d4
Puneet Sharma <puni199711@gmail.com>
2026-03-31 09:44:17 +0530

    tdasdaj

--- types/editorjs.d.ts
+++ types/editorjs.d.ts
@@ -1,4 +1,98 @@
+// Core Editor.js
+declare module "@editorjs/editorjs" {
+  const EditorJS: any;
+  export default EditorJS;
+}
+
+// Common Tools
+declare module "@editorjs/header" {
+  const Header: any;
+  export default Header;
+}
+
+declare module "@editorjs/paragraph" {
+  const Paragraph: any;
+  export default Paragraph;
+}
+
+declare module "@editorjs/list" {
+  const List: any;
+  export default List;
+}
+
+declare module "@editorjs/image" {
+  const ImageTool: any;
+  export default ImageTool;
+}
+
 declare module "@editorjs/embed" {
   const Embed: any;
   export default Embed;
+}
+
+declare module "@editorjs/table" {
+  const Table: any;
+  export default Table;
+}
+
+declare module "@editorjs/code" {
+  const Code: any;
+  export default Code;
+}
+
+declare module "@editorjs/quote" {
+  const Quote: any;
+  export default Quote;
+}
+
+declare module "@editorjs/delimiter" {
+  const Delimiter: any;
+  export default Delimiter;
+}
+
+// Inline Tools
+declare module "@editorjs/marker" {
+  const Marker: any;
+  export default Marker;
+}
+
+declare module "@editorjs/inline-code" {
+  const InlineCode: any;
+  export default InlineCode;
+}
+
+declare module "@editorjs/link" {
+  const LinkTool: any;
+  export default LinkTool;
+}
+
+// Optional / Advanced Tools
+declare module "@editorjs/checklist" {
+  const Checklist: any;
+  export default Checklist;
+}
+
+declare module "@editorjs/warning" {
+  const Warning: any;
+  export default Warning;
+}
+
+declare module "@editorjs/raw" {
+  const RawTool: any;
+  export default RawTool;
+}
+
+declare module "@editorjs/simple-image" {
+  const SimpleImage: any;
+  export default SimpleImage;
+}
+
+declare module "@editorjs/attaches" {
+  const Attaches: any;
+  export default Attaches;
+}
+
+declare module "@editorjs/personality" {
+  const Personality: any;
+  export default Personality;
 }
